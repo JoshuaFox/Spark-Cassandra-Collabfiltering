2759e2f07018c15e8267e026664a42fd65a7d128
JoshuaFox <joshuafox@users.noreply.github.com>
2014-11-27 11:28:33 +0200

    csv

--- src/sql/collab_filter_setup.sql
+++ src/sql/collab_filter_setup.sql
@@ -1,3 +1,4 @@
+--set up database
 DROP KEYSPACE IF EXISTS employerratings;
 CREATE KEYSPACE employerratings WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};
 CREATE TABLE employerratings.ratings (rating_id TIMEUUID PRIMARY KEY, user_id INT, item_id INT, rating DOUBLE);
